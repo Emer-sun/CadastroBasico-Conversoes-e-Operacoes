7074758a44a253146bae7b8c438ee650b642e4ca
Emer-sun <otsutsuki_maroki@hotmail.com>
2025-06-27 00:25:04 -0300

    Sistema simples em C# para treinar entrada de dados, tipos primitivos, conversões, operações aritméticas e formatação. Desenvolvido como parte do módulo inicial de estudos.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Olá, por gentileza digite seu nome"); //Instrução para o usuário
+string nome =  Console.ReadLine();
+Console.WriteLine($"Olá {nome}, um prazer ter você por aqui, por gentileza digite sua idade");
+int idade = Convert.ToInt32(Console.ReadLine()); //Armeza informação
+int dobro = idade * 2;
+double media = (idade + dobro) / 2.0; //especificar o .0 pois se não sai como numero inteiro
+int resto = (idade % dobro);
+Console.WriteLine("Digite sua altura");
+double altura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite sua nota da prova");
+float nota = float.Parse(Console.ReadLine()); //usado .parse para conversão pois a classe Convert não possui converção para float
+int arredondamento = (int)nota;
+Console.WriteLine($"Digite sua data de nascimento");
+DateTime nascimento = Convert.ToDateTime(Console.ReadLine());
+
+
+Console.WriteLine($"Olá, {nome}");
+Console.WriteLine($"Você tem {idade} e mede {altura}");
+Console.WriteLine($"Sua nota final foi {nota}, arrendondando para {arredondamento}");
+Console.WriteLine($"Você nasceu em {nascimento.ToShortDateString()}"); //Usando o ToShort para comprimir a informação e não ficar os 00:00
+Console.WriteLine($"Idade calculada {idade} | Dobro da idade {dobro}" );
+Console.WriteLine($"Média entre idade digitada e calculada: {media}");
+Console.WriteLine($"Resto da divisão entre idade e nota convertida {resto}");
+Console.ReadKey();
